bc90107b5067a8e4c93d1eb58bfdf047b778d33e
Ahmad Bakare <65125857+adelekeahmad@users.noreply.github.com>
2022-08-25 01:39:10 -0700

    Add project files.

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace exceptionHandling;
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            string secretWord = "Chelsea";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOfGuesses = false;
+            while (guess != secretWord && !outOfGuesses)
+            {
+
+                if (guessCount < guessLimit)
+                {
+                    Console.Write("Who wins today's match: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }
+
+                else
+                {
+                    outOfGuesses = true;
+                }
+
+            }
+            if (outOfGuesses)
+            {
+                Console.Write("you lose!");
+            }
+            else
+            {
+                //if condition does not hold
+                Console.Write("you win!");
+            }
+        }
+        catch
+        {
+            Console.WriteLine("GoodBye");
+        }
+        
+        Console.ReadLine();
+    }
+}
